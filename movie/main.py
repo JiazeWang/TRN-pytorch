98d09ab816cf9d4d8cb20bea2daa1351257638b0
JiazeWang <jiazewang@csu.edu.cn>
2019-11-06 22:17:17 +0800

    1

--- movie/main.py
+++ movie/main.py
@@ -260,7 +260,8 @@ def validate(val_loader, model, criterion, logger=None):
         #label_path = '../results/labels.npy'
         #label_path = 'data/ceshi.npy'
         #label_path = 'data/val.npy'
-        label_path = '/home/jzwang/code/RGB-FLOW/MovieNet/data/val.npy'
+        #label_path = '/home/jzwang/code/RGB-FLOW/MovieNet/data/val.npy'
+        label_path = '/home/jzwang/code/RGB-FLOW/MovieNet/data/new/movie_val.npy'
         labels = np.load(label_path)
         mAP, wAP = compute_map(labels, output_mtx)
 
